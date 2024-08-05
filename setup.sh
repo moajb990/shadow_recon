e5325ea816e30b6d9536449fbd6db2b2d249449b
moajb990 <86415810+moajb990@users.noreply.github.com>
2024-08-05 17:25:28 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#version
+version="1.0.0"
+#save things
+echo "saving user and main path."
+user=$(logname)
+main_path=$(pwd)
+###########
+# install python
+echo "installing python"
+sudo apt-get install python3
+#download dirsearch
+echo "installing dirsearch from github"
+sleep 1.5
+sudo git clone https://github.com/maurosoria/dirsearch.git
+#move the dirsearch to default path
+echo "setup dirsearch and another tools"
+sleep 1.5
+sudo mv dirsearch /home/$user/Desktop
+cd /home/$user/dirsearch && pip install -r requirements.txt
+sudo apt-get install curl
+sudo apt-get install jq
+###########################
+# make shadow recon work without bash or ./
+echo "move the shadowrecon and lib to /usr/local/bin"
+sleep 1
+cd $main_path
+sudo mkdir SHR /usr/local/bin
+sudo chmod +x shadow_recon.sh
+sudo chmod +x scan_lib
+sudo mv shadow_recon.sh /usr/local/bin/SHR/shadow_recon
+sudo mv scan_lib /usr/local/bin/SHR/
+sleep 1.5
+rm -rf $main_path
